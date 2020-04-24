814e2f5b11bcd13b8fb11f52c9bb3c2a572644a4
pameladilly <pameladilly@hotmail.com>
2020-04-24 11:58:20 -0300

    Aulas dia 24/04

--- minhasfinancas/src/test/java/com/pameladilly/minhasfinancas/service/UsuarioServiceTest.java
+++ minhasfinancas/src/test/java/com/pameladilly/minhasfinancas/service/UsuarioServiceTest.java
@@ -1,5 +1,6 @@
 package com.pameladilly.minhasfinancas.service;
 
+import java.security.Provider.Service;
 import java.util.Optional;
 
 import org.aspectj.apache.bcel.util.Repository;
@@ -12,6 +13,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.expression.spel.ast.OpPlus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -26,17 +28,12 @@ import com.pameladilly.minhasfinancas.service.impl.UsuarioServiceImpl;
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-	
-	UsuarioService service;
+	@SpyBean
+	UsuarioServiceImpl service;
 	
 	@MockBean
 	UsuarioRepository repository;
-	
-	@Before
-	public void setUp() {
-		service = new UsuarioServiceImpl(repository);
-		
-	}
+
 	
 	@Test(expected = Test.None.class)
 	public void deveAutenticarUmUsuarioComSucesso() {
@@ -52,23 +49,29 @@ public class UsuarioServiceTest {
 		
 	}
 	
-	@Test(expected = ErroAutenticacao.class)
+	@Test
 	public void deveLancarErroQuandoNaoEncontrarUsuarioCadastradoComOEmailInformado() {
 		
 		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
 		
-		service.autenticar("usuario@email.com", "senha");
+		Throwable exception = Assertions.catchThrowable(() -> service.autenticar("usuario@email.com", "senha"));
+		
+		Assertions.assertThat(exception).isInstanceOf(ErroAutenticacao.class).hasMessage("Usuário não encontrado para o email informado.");
+		
 		
 	}
 	
-	@Test(expected = ErroAutenticacao.class)
+	@Test
 	public void deveLancarErroQuandoSenhaNaoBater() {
 		String senha = "senha";
 		
 		Usuario usuario = Usuario.builder().email("usuario@email.com").senha(senha).build();
 		Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
 		
-		service.autenticar("usuario@email.com", "123");
+		
+		Throwable exception = Assertions.catchThrowable(() -> service.autenticar("usuario@email.com", "123"));
+		Assertions.assertThat(exception).isInstanceOf(ErroAutenticacao.class).hasMessage("Senha inválida.");
+		
 	}
 	@Test(expected = Test.None.class)
 	public void deveValidarEmail() {
@@ -76,6 +79,38 @@ public class UsuarioServiceTest {
 		Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 		
 		service.validarEmail("usuario@email.com");
+	}
+	
+	@Test
+	public void deveSalvarUmUsuario() {
+		Mockito.doNothing().when(service).validarEmail(Mockito.anyString());
+		
+		Usuario usuario = Usuario.builder().id(1l).nome("nome").email("usuario@email.com").senha("senha").build();
+		
+		Mockito.when(repository.save(Mockito.any(Usuario.class))).thenReturn(usuario);
+		
+		Usuario usuarioSalvo = service.salvarUsuario(new Usuario());
+		
+		Assertions.assertThat(usuarioSalvo).isNotNull();
+		Assertions.assertThat(usuarioSalvo.getId()).isEqualTo(1l);
+		Assertions.assertThat(usuarioSalvo.getNome()).isEqualTo("nome");
+		Assertions.assertThat(usuarioSalvo.getEmail()).isEqualTo("usuario@email.com");
+		Assertions.assertThat(usuarioSalvo.getSenha()).isEqualTo("senha");		
+		
+	}
+	
+	@Test(expected = RegraNegocioException.class)
+	public void naoDeveSalvarUmUsuarioComEmailJaCadastrado() {
+		
+		String email = "usuario@email.com";
+		
+		Usuario usuario = Usuario.builder().email(email).build();
+		
+		Mockito.doThrow(RegraNegocioException.class).when(service).validarEmail(email);
+		
+		service.salvarUsuario(usuario);
+		
+		Mockito.verify(repository, Mockito.never()).save(usuario);
 	}
 	
 	@Test(expected = RegraNegocioException.class)
