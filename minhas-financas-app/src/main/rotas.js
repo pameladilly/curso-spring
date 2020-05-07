a00ad06e32d65939578d6a8e37e695578d9fa1e1
pameladilly <pameladilly@hotmail.com>
2020-05-07 11:09:12 -0300

    Implementação das telas iniciais

---
+++ minhas-financas-app/src/main/rotas.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Login from '../views/login'
+import cadastroUsuario from '../views/cadastroUsuario'
+
+import {
+    Route,
+    Switch,
+    HashRouter
+} from 'react-router-dom'
+import CadastroUsuario from '../views/cadastroUsuario'
+
+function Rotas() {
+
+    return (
+        <HashRouter>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/cadastro-usuarios" component={CadastroUsuario} />
+            </Switch>
+        </HashRouter>
+    )
+}
+
+export default Rotas
--- minhas-financas-app/src/views/login.js
+++ minhas-financas-app/src/views/login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from '../components/card'
 import FormGroup from '../components/form-group'
+import { withRouter } from 'react-router-dom'
 
 class Login extends React.Component {
 
@@ -13,53 +14,56 @@ class Login extends React.Component {
         console.log(`Senha : ${this.state.senha}`)
     }
 
+    prepareCadastrar = () => {
+        this.props.history.push('/cadastro-usuarios')
+    }
+
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6" style={{ position: 'relative', left: '300px' }} >
-                        <div className="bs-docs-section">
-                            <Card title="Login">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="bs-component">
-                                            <fieldset>
-                                                <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
-                                                    <input type="email"
-                                                        value={this.state.email}
-                                                        onChange={e => this.setState({ email: e.target.value })}
-                                                        className="form-control"
-                                                        id="exampleInputEmail1"
-                                                        aria-describedby="emailHelp"
-                                                        placeholder="Digite o Email" />
+            <div className="row">
+                <div className="col-md-6" style={{ position: 'relative', left: '300px' }} >
+                    <div className="bs-docs-section">
+                        <Card title="Login">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="bs-component">
+                                        <fieldset>
+                                            <FormGroup label="Email: *" htmlFor="exampleInputEmail1">
+                                                <input type="email"
+                                                    value={this.state.email}
+                                                    onChange={e => this.setState({ email: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputEmail1"
+                                                    aria-describedby="emailHelp"
+                                                    placeholder="Digite o Email" />
 
-                                                </FormGroup>
-                                                <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
-                                                    <input type="password"
-                                                        value={this.state.senha}
-                                                        onChange={e => this.setState({senha: e.target.value})}
-                                                        className="form-control"
-                                                        id="exampleInputPassword1"
-                                                        placeholder="Password" />
+                                            </FormGroup>
+                                            <FormGroup label="Senha: *" htmlFor="exampleInputPassword1">
+                                                <input type="password"
+                                                    value={this.state.senha}
+                                                    onChange={e => this.setState({ senha: e.target.value })}
+                                                    className="form-control"
+                                                    id="exampleInputPassword1"
+                                                    placeholder="Password" />
 
-                                                </FormGroup>
-                                                <button onClick={this.entrar} className="btn btn-success">Entrar</button>
-                                                <button className="btn btn-danger">Cadastrar</button>
+                                            </FormGroup>
+                                            <button onClick={this.entrar} className="btn btn-success">Entrar</button>
+                                            <button onClick={this.prepareCadastrar} className="btn btn-danger">Cadastrar</button>
 
-                                            </fieldset>
+                                        </fieldset>
 
-                                        </div>
                                     </div>
-
                                 </div>
-                            </Card>
-                        </div>
+
+                            </div>
+                        </Card>
                     </div>
                 </div>
             </div>
+
         )
     }
 
 }
 
-export default Login
+export default withRouter(Login)
