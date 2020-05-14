15f1461aaf1333379e6909c3d36d89cb801499eb
pameladilly <pameladilly@hotmail.com>
2020-05-14 08:37:10 -0300

    Criação de metódo para buscar lançamentos a partir do iD

--- minhasfinancas/src/main/java/com/pameladilly/minhasfinancas/api/resource/LancamentoResource.java
+++ minhasfinancas/src/main/java/com/pameladilly/minhasfinancas/api/resource/LancamentoResource.java
@@ -114,6 +114,27 @@ public class LancamentoResource {
 		List<Lancamento> lancamentos = service.buscar(lancamentoFiltro);
 		return ResponseEntity.ok(lancamentos);
 	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity obterLancamento(@PathVariable ("id") Long id) {
+		return service.obterPorId(id)
+				.map( lancamento -> 
+					new ResponseEntity(converter(lancamento), HttpStatus.OK)
+				).orElseGet( () -> new ResponseEntity(HttpStatus.NOT_FOUND));
+	}
+	
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder()
+				.id(lancamento.getId())
+				.descricao(lancamento.getDescricao())
+				.valor(lancamento.getValor())
+				.mes(lancamento.getMes())
+				.ano(lancamento.getAno())
+				.status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name())
+				.usuario(lancamento.getUsuario().getId())
+				.build();
+	}
 
 	@DeleteMapping("{id}")
 	public ResponseEntity deletar(@PathVariable("id") Long id) {
