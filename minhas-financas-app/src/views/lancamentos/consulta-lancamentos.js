e3949f3b110ca89340caa4fb622fbf7cf800913c
pameladilly <pameladilly@hotmail.com>
2020-05-14 08:36:08 -0300

    Aula di 14/05 -  Cadastro de lançamentos

--- minhas-financas-app/src/views/lancamentos/consulta-lancamentos.js
+++ minhas-financas-app/src/views/lancamentos/consulta-lancamentos.js
@@ -58,7 +58,7 @@ class ConsultaLancamentos extends React.Component {
     }
 
     editar = (id) => {
-        console.log('Editando o lançamento', id)
+        this.props.history.push(`/cadastro-lancamentos/${id}`)
     }
 
     abrirConfirmar = (lancamento) => {
@@ -87,6 +87,9 @@ class ConsultaLancamentos extends React.Component {
             })
     }
 
+    preparaFormularioCadastro = () => {
+        this.props.history.push('/cadastro-lancamentos')
+    }
 
 
     render() {
@@ -146,7 +149,7 @@ class ConsultaLancamentos extends React.Component {
                             </FormGroup>
 
                             <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
-                            <button type="button" className="btn btn-danger" >Cadastrar</button>
+                            <button onClick={this.preparaFormularioCadastro} type="button" className="btn btn-danger" >Cadastrar</button>
 
                         </div>
                     </div>
